69ff87f478083751a47a47a71116662d223d7c7d
aditya <adiyakothekar79.com>
2024-02-04 05:42:01 -0800

    update

--- Bakery_Python/settings.py
+++ Bakery_Python/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR,'/static'),)
+STATIC_DIRS = (os.path.join(BASE_DIR,'/static'),)
 STATIC_ROOT = os.path.join(BASE_DIR,'/static')
 
 
